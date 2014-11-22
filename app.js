67b3e7a687724a6f86fa6bf64c323d2228fa2dc7
Rudi MK <rudimk.firefly@gmail.com>
2014-11-22 22:25:21 +0000

    Added basic support for running Julia code from the API.

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.get('/', function(req, res){
 res.json({'message': 'Welcome to the basic JuliaDroid backend. Hit /api/0.1/compute with a Julia expression, for best results.'});
 });
 
+
+app.get('/api/0.1/compute/:julia_expr', function(req, res){
+	julia_expr_result = julia.eval(req.param("julia_expr"));
+	res.json({'result': julia_expr_result});
+});
+
 var server = app.listen(3000, function() {
 console.log('The JuliaDroid backend is now listening on port %d', server.address().port);
 });
